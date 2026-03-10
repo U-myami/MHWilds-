059fb8d6c831424ed3fc585afabfa7fd48e096d3
U-myami <litalico.tanino@gmail.com>
2026-03-10 22:15:10 +0900

    Delete monster-hunter-wilds directory

--- monster-hunter-wilds/data/map.js
+++
@@ -1,9 +0,0 @@
-const MAPS = [
-  {
-    id: "ice-region",
-    name: "氷結地帯",
-    environment: "極寒",
-    monsters: ["velkhana"],
-    materials: ["氷晶石", "霜降り草"]
-  }
-];
--- monster-hunter-wilds/js/common.js
+++
@@ -1,5 +0,0 @@
-function savePost(name, text) {
-  const posts = JSON.parse(localStorage.getItem('mh_posts') || '[]');
-  posts.push({ name, text, date: new Date().toLocaleString() });
-  localStorage.setItem('mh_posts', JSON.stringify(posts));
-}
